c5a34729f3d46907c8e1aac5b6c8fd87ce785146
Victor Langat <provicml@gmail.com>
2023-10-26 12:28:16 +0300

    added blueprints

---
+++ api/v1/app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" app module"""
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(err):
+    """teardown"""
+    return storage.close()
+
+
+@app.errorhandler(404)
+def error_404(err):
+    """handle errors"""
+    return jsonify({'error': "Not found"}), 404
+
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" index module"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status_ok():
+    """return status JSON"""
+    return jsonify({"status": "ok"})
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stor_count():
+    """return number of each objects by type: """
+    count = {
+            'amenities': storage.count("Amenity"),
+            'cities': storage.count("City"),
+            'places': storage.count("Place"),
+            'reviews': storage.count("Review"),
+            'states': storage.count("State"),
+            'users': storage.count("User")
+            }
+    return jsonify(count)
