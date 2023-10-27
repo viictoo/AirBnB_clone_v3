d2d4a7f387cd164ae48a103593212b45a2a1e1a0
Victor Langat <provicml@gmail.com>
2023-10-27 18:29:33 +0300

    test fix 5: cors?

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,8 +8,8 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
